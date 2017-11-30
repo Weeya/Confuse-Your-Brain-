5120145d2520be7b77cb7d2ddf74c28e8540f83c
Weeya Weerawatpruchya <weeyaa.w@gmail.com>
2017-12-01 02:21:44 +0700

    random color rect

--- core/src/com/mygdx/game/BrainGame.java
+++ core/src/com/mygdx/game/BrainGame.java
@@ -1,9 +1,13 @@
 package com.mygdx.game;
 
 import java.awt.Shape;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -14,23 +18,57 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class BrainGame extends Game{
-	private RectRenderer rectRenderer;
-	private Texture texture;
+	
 	public SpriteBatch batch;
-	private ShapeRenderer shapeRenderer;
 	ShapeRenderer shape;
 	BitmapFont font;
+	
+	List<Color> listcolor;
+	Random rand;
+	int tmpposlist;
+	Color tmpcolor1;
+	Color tmpcolor2;
+	Color tmpcolor3;
+	Color tmpcolor4;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		setScreen(new GameScreen(this));
 		shape = new ShapeRenderer();
-		
 		font = new BitmapFont();
 		font.getData().setScale(3);
 		font.setColor(0, 0, 0, 1);
+		
+		//code of dondon
+		rand  = new Random();
+		listcolor = new ArrayList();
+		listcolor.add(Color.SALMON);
+		listcolor.add(Color.LIME);
+		listcolor.add(Color.CORAL);
+		listcolor.add(Color.VIOLET);
+		randcolor();
+		
+		
 		//font.getRegion().getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
+	}
+	public void randcolor() {
+		tmpposlist = rand.nextInt(listcolor.size());
+		tmpcolor1 = listcolor.get(tmpposlist);
+		listcolor.remove(tmpposlist);
+		tmpposlist = rand.nextInt(listcolor.size());
+		tmpcolor2 = listcolor.get(tmpposlist);
+		listcolor.remove(tmpposlist);
+		tmpposlist = rand.nextInt(listcolor.size());
+		tmpcolor3 = listcolor.get(tmpposlist);
+		listcolor.remove(tmpposlist);
+		tmpposlist = rand.nextInt(listcolor.size());
+		tmpcolor4 = listcolor.get(tmpposlist);
+		listcolor.remove(tmpposlist);
+		listcolor.add(Color.SALMON);
+		listcolor.add(Color.LIME);
+		listcolor.add(Color.CORAL);
+		listcolor.add(Color.VIOLET);
 	}
 	
 	public void render () {
@@ -46,28 +84,32 @@ public class BrainGame extends Game{
 		*/
 		batch.end();
 		
-		shape.begin(ShapeType.Filled);
-		shape.setColor(218/255f, 68/255f, 83/255f, 0);
-		shape.rect(160, 250, 130, 130);
-		
-		shape.setColor(246/255f, 187/255f, 66/255f, 0);
-		shape.rect(320, 250, 130, 130);
-		
-		shape.setColor(140/255f, 193/255f, 82/255f, 0);
-		shape.rect(160, 420, 130, 130);
-		
-		shape.setColor(59/255f, 175/255f, 218/255f, 0);
-		shape.rect(320, 420, 130, 130);
-		
-		shape.end();
-		
-	
+	//	if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+			randcolor();
+		}
+
+			shape.begin(ShapeType.Filled);
+			shape.setColor(tmpcolor1);
+			shape.rect(160, 250, 130, 130);
+			shape.setColor(tmpcolor2);
+			shape.rect(320, 250, 130, 130);
+			shape.setColor(tmpcolor3);
+			shape.rect(160, 420, 130, 130);
+			shape.setColor(tmpcolor4);
+			shape.rect(320, 420, 130, 130);
+			shape.end();
+			
+	//	}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		font.dispose();
-		//shapeRenderer.dispose();
+	}
+	
+	public void update(float delta) {
+		
 	}
 }
